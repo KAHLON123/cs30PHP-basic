53df20d31897bffaec31b628657fd79e158ddac5
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-02-02 11:14:45 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,10 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP practice</title>
+    <title>PHP math</title>
 </head>
 <body>
-    <h1>Hello</h1>
-    <p><?php echo "hi" ?></p>
+    <h1>Hello! Lets do some math.</h1>
+    <h2><?php echo "Here's the question:<br><br>
+    if M / 5 has a remainder of 2,<br>
+    and N / 5 has a remainder of 4,<br>
+    what is the remainder of (M + N) / 5?" ?></h2>
+<?php
+/*
+if M % 5 = 2
+and N % 5 = 4
+what is M + N % 5?
+*/
+$testN = 0;
+$testM = 0;
+
+do {
+    $testM++;
+    if ($testM % 5 == 2){
+        echo "M = ", $testM;
+        break;
+    }
+} while ($testM % 5 != 2);
+
+do {
+    $testN++;
+    if($testN % 5 == 4){
+        echo "N = ", $testN;
+        break;
+    } 
+} while ($testN % 5 != 4);
+
+?>
+
 </body>
 </html>
