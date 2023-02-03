bb4073067efb39563cac08c0d2e1eb68aa1aa8cf
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-02-03 11:23:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP math</title>
+    <title>PHP sandbox</title>
 </head>
 <body>
     <h1>Hello! Lets do some math.</h1>
@@ -24,7 +24,7 @@ $testM = 0;
 do {
     $testM++;
     if ($testM % 5 == 2){
-        echo "M = ", $testM;
+        echo "M = ", $testM, "<br />";
         break;
     }
 } while ($testM % 5 != 2);
