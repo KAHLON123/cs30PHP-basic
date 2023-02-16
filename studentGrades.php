316bff35e3c5e8401423c95cb1aaf1e81ac6801d
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-02-16 11:23:27 -0700

    Update studentGrades.php

--- studentGrades.php
+++ studentGrades.php
@@ -3,19 +3,11 @@
     <title>Student Grades</title>
 </head>
 <body>
-    <?php
-    $grades = array();
-    for ($n = 0; $n < 35; $n++){
-        $randNum = rand(0, 100);
-        array_push($grades, $randNum);
-    }
-    echo var_dump($grades);
-    ?>
     <section>
         <h1>Student Grades</h1>
         <form action="studentGrades.php" method="POST">
             <label>Options:</label><br>
-            <select>
+            <select name="s">
                 <option value="all-grades">Display All Grades</option>
                 <option value="honours">Display Honours</option>
                 <option value="stats">Stats</option>
@@ -25,32 +17,52 @@
             <input type="submit" value="submit" name="submit">
     </section>
     <?php
+    $grades = randomGradeArray();
     if (isset($_POST['submit'])){
-        if (s == $_POST['all-grades']) {
-            echo allGrades();
-        } elseif (s == $_POST['honours']){
-            echo honours();
-        } elseif (s == $_POST['stats']){
-            echo stats();
-        } elseif (s == $_POST['random']){
-            echo random();
-        } elseif (s == $_POST['exit']){
+        $s = $_POST['s'];
+        if ($s == 'all-grades') {
+            echo allGrades($grades);
+        } elseif ($s == 'honours'){
+            echo honours($grades);
+        } elseif ($s == 'stats'){
+            echo stats($grades);
+        } elseif ($s == 'random'){
+            echo randomGradeArray();
+            echo "GRADES HAVE BEEN RANDOMIZED";
+        } elseif ($s == 'exit'){
             echo "Bye-bye";
         }
     }
-    function allGrades(){
+    function allGrades($grades){
+        echo 'ALL GRADES <br />';
         for ($n = 0; $n < count($grades); $n++){
-            echo $grades[$n];
+            echo $grades[$n] . '<br />';
         }
     }
-    function honours(){
-
+    function honours($grades){
+        for ($n = 0; $n< count($grades); $n++){
+            if ($grades[$n] > 80){
+                echo $grades[$n], '<br />';
+            }
+        }
     }
-    function stats(){
-
+    function stats($grades){
+        $highest = 1;
+        $lowest = 100;
+        for ($n = 0; $n < count($grades); $n++){
+            if ($grades[$n] > $highest){
+                $highest = $grades[$n];
+            } elseif ($grades[$n] < $lowest)
+        }
+        echo $highest;
     }
-    function random(){
-
+    function randomGradeArray(){
+        $temp = [];
+        for ($n = 0; $n < 35; $n++){
+            $randNum = rand(0, 100);
+            array_push($temp, $randNum);
+        }
+        return $temp;
     }
     ?>
 </body>
