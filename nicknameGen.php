fc194f1281341c603ec3fbda244d0a0abaa8049a
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-02-27 11:22:47 -0700

    Update nicknameGen.php

--- nicknameGen.php
+++ nicknameGen.php
@@ -1,3 +1,16 @@
+<?php
+
+    $conn = mysqli_connect('localhost', 'nicknameUser', 'localhost', 'nickname_gen');
+
+    if (!$conn){
+        echo 'Connection error: ' . mysqli_connect_error();
+    }
+
+    $sql = 'SELECT * FROM user_name'
+    $result = mysqli_query($conn, $sql);
+    $name = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
 <!DOCTYPE html>
 <head>
     <title>Student Grades</title>
@@ -23,22 +36,22 @@
     <h2>See your nicknames below:</h2>
 </section>
 <?php
-if (isset($_GET['submit'])){
-    $selection = $_GET['s'];
-    if ($selection ==){
+// if (isset($_GET['submit'])){
+//     $selection = $_GET['s'];
+//     if ($selection ==){
 
-    } elseif ($selection ==){
+//     } elseif ($selection ==){
 
-    } elseif ($selection ==){
+//     } elseif ($selection ==){
         
-    } elseif ($selection ==){
+//     } elseif ($selection ==){
         
-    } elseif ($selection ==){
+//     } elseif ($selection ==){
         
-    } elseif ($selection ==){
+//     } elseif ($selection ==){
         
-    }
-}
+//     }
+// }
 ?>
 
 </body>
