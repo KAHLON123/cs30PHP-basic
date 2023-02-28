f3332df50d22ad7a7a371fbf47b102d5d0c4ff64
KAHLON123 <h.kahlon5@share.epsb.ca>
2023-02-28 11:22:20 -0700

    Update nicknameGen.php

--- nicknameGen.php
+++ nicknameGen.php
@@ -1,15 +1,19 @@
-<?php
+<!-- 
+    SQL FOR USERS NAME
+    TEXT FILE FOR NICKNAMES
+-->
 
+<?php
+//  connect and check connection
     $conn = mysqli_connect('localhost', 'nicknameUser', 'localhost', 'nickname_gen');
 
     if (!$conn){
         echo 'Connection error: ' . mysqli_connect_error();
     }
 
-    $sql = 'SELECT * FROM user_name'
+    $sql = 'SELECT * FROM user_name';
     $result = mysqli_query($conn, $sql);
     $name = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
 ?>
 <!DOCTYPE html>
 <head>
@@ -33,25 +37,55 @@
         <option value="remove">Remove a Nickname</option>
         <option value="exit">Exit</option>
     </select>
+    <input type="submit" name="submit" value="submit">
     <h2>See your nicknames below:</h2>
+    <form>
+        <input type="text" name="add-nick">
+        <input type="text" name="remove-nick"><br />
+        <label>ADD or REMOVE a nickname</label>
+    </form>
 </section>
+
 <?php
-// if (isset($_GET['submit'])){
-//     $selection = $_GET['s'];
-//     if ($selection ==){
+$nicknamesArr = file("nicknames.txt", NULL);
+//var_dump($nicknamesArr);
 
-//     } elseif ($selection ==){
-
-//     } elseif ($selection ==){
+var_dump($name);
+if (isset($_GET['submit'])){
+    $selection = $_GET['s'];
+    if ($selection == "change"){
+        changeName($_GET['first'], $_GET['last']);
+    } elseif ($selection == "display-rand"){
+        displayRand();
+    }
+//     } elseif ($selection == "display-all"){
         
-//     } elseif ($selection ==){
+//     } elseif ($selection == "add"){
         
-//     } elseif ($selection ==){
+//     } elseif ($selection == "remove"){
         
-//     } elseif ($selection ==){
+//     } elseif ($selection == "exit"){
         
 //     }
-// }
+ }
+
+function changeName($first, $last){
+    $addToSql = "UPDATE user_name SET first=$first, last=$last";
+    if (mysqli_query($conn, $addToSql)){
+        echo "Name updated successfully";
+    } else {
+        echo "Error updating name: " . mysqli_error($conn);
+    }
+}
+function displayRand(){
+    for ($n = 0; $n < $nicknames; $n++){
+        echo $name[1], $nicknames[$n], $name[1], "<br />";
+
+    }
+
+}
+
+mysqli_close($conn);
 ?>
 
 </body>
